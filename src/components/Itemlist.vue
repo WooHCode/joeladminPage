24e672faf35607c9231d286d2ecb9d726b1fa776
WooHCode <gusdn526@naver.com>
2023-01-04 17:56:26 +0900

    feature itemlist

---
+++ src/components/Itemlist.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+import axios from 'axios';
+import { relative } from 'path';
+export default {
+    setup() {
+        const state = relative({
+            items: [],
+            form: {
+                name: '',
+            }
+        })
+
+        const load = () => {
+            axios.get(`/api/v2/items`).then(({ data }) => {
+                console.log(data);
+                state.items = data;
+            })
+        }
+        load();
+        return {state, }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
