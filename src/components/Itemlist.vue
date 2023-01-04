bc2cbd8f510c4c6962839a8062baf5473ff66f3a
WooHCode <gusdn526@naver.com>
2023-01-04 19:51:02 +0900

    update items

--- src/components/Itemlist.vue
+++ src/components/Itemlist.vue
@@ -1,28 +1,61 @@
 <template>
-
+    <div class="items">
+        <div class="container">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>상품명</th>
+                        <th>가격</th>
+                        <th>상품 사진 경로</th>
+                        <th>상품설명</th>
+                        <th>상품코드</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(i, idx1) in state.items" :key="idx1">
+                        <td>{{ state.items.length - idx1 }}</td>
+                        <td>{{ i.name }}</td>
+                        <td>{{ i.price }}</td>
+                        <td>{{ i.imgPath }}</td>
+                        <td>{{ i.imgDes }}</td>
+                        <td>{{ i.itemCode }}</td>
+                        <td>
+                            <div v-for="(i, idx2) in o.items" :key="idx2">{{ i.name }}</div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
-import axios from 'axios';
-import { relative } from 'path';
+import { reactive } from '@vue/reactivity'
+import axios from 'axios'
+
 export default {
+    name: "Itemlist",
+    props: {
+        items : Object,
+    },
     setup() {
-        const state = relative({
+        const state = reactive({
             items: [],
-            form: {
-                name: '',
-            }
         })
 
-        const load = () => {
-            axios.get(`/api/v2/items`).then(({ data }) => {
-                console.log(data);
-                state.items = data;
-            })
-        }
-        load();
-        return {state, }
-    },
+        axios.get("/api/v1/items").then(({ data }) => {
+            state.items = [];
+
+            for (let d of data) {
+                if (d.items) {
+                    d.items = JSON.parse(d.items);
+                }
+                state.items.push(d);
+            }
+        })
+        return { state };
+    }
 }
 </script>
 
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -7,14 +7,14 @@ import Client from "@/pages/Client"
 import Store from "@/pages/Store"
 import { createRouter, createWebHistory } from "vue-router"
 
-const routes =[
-    {path: '/', component: Home},
-    {path: '/login', component: Login},
-    {path: '/sales', component: Sales},
-    {path: '/items', component: Items},
-    {path: '/emp', component: Emp},
-    {path: '/client', component: Client},
-    {path: '/store', component: Store},
+const routes = [
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
+    { path: '/sales', component: Sales },
+    { path: '/items', component: Items },
+    { path: '/emp', component: Emp },
+    { path: '/client', component: Client },
+    { path: '/store', component: Store },
 ]
 
 const router = createRouter({
