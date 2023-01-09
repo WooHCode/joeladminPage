8d7b521ef8ffed27d2374e49e59f398828396d6c
WooHCode <gusdn526@naver.com>
2023-01-09 22:34:51 +0900

    상품추가 기능

--- src/components/Itemmodal.vue
+++ src/components/Itemmodal.vue
@@ -7,36 +7,40 @@
                 <h4 class="mb-3">상품 정보</h4>
                 <div class="needs-validation" novalidate="">
                     <div class="row g-3">
-                        <div class="col-12"><label for="itemName" class="form-label"></label>
+                        <div class="col-12">
                             <input type="text" class="form-control" id="itemName" placeholder="상품명"
                                 v-model="state.form.name">
                         </div>
-                        <div class="col-12"><label for="address" class="form-label"></label>
-                            <input type="number" class="form-control" id="address" placeholder="상품 가격"
-                                v-model="state.form.address">
+                        <div class="col-12">
+                            <input type="number" class="form-control" id="itemPrice" placeholder="상품 가격"
+                                v-model="state.form.price">
+                        </div>
+                        <div class="col-12">
+                            <input type="text" class="form-control" id="imgPath" placeholder="이미지 경로"
+                                v-model="state.form.imgPath">
                         </div>
                     </div>
                     <hr class="my-4">
                     <h4 class="mb-3">상품상세정보</h4>
                     <hr class="my-4">
+                    <label for="cc-name" class="form-label">상품 코드</label>
                     <div class="my-3">
-                        <label for="cc-name" class="form-label">상품 코드</label>
-                        <div class="form-check">
-                            <input id="itemCode" name="paymentMethod" type="checkbox" class="form-check-input"
-                                value="itemCode" v-model="state.form.payment">
-                            <label class="form-check-label" for="itemCode">MILK</label>
-                        </div>
-                        <div class="form-check">
-                            <input id="itemCode" name="paymentMethod" type="checkbox" class="form-check-input"
-                                value="itemCode" v-model="state.form.payment">
-                            <label class="form-check-label" for="itemCode">MILK</label>
-                        </div>
+                        <select name="itemCode" required v-model="state.form.itemCode">
+                            <option value="" disabled selected>상품선택</option>
+                            <option value="MILK">MILK</option>
+                            <option value="WHITE">WHITE</option>
+                            <option value="BLACK">BLACK</option>
+                            <option value="ADE">ADE</option>
+                            <option value="TEA">TEA</option>
+                            <option value="B_TEA">B_TEA</option>
+                        </select>
                     </div>
-                    <label for="cc-name" class="form-label">상품 설명</label>
-                    <input type="text" class="form-control" id="cc-name" v-model="state.form.cardNumber">
+                    <label for="itemDes" class="form-label">상품 설명</label>
+                    <input type="text" class="form-control" id="itemDes" v-model="state.form.itemDes">
                     <hr class="my-4">
-                    <button @click="modalHide($emit,modalHide)">닫기</button>
-                    <button class="w-50 btn btn-primary btn-lg" @click="submit(), modalHide($emit,modalHide)">상품 추가하기</button>
+                    <button class="w-50 btn btn-primary btn-lg" @click="[submit(), modalHide($emit, modalHide)]">상품
+                        추가하기</button>
+                    <button @click="modalHide($emit, modalHide)">닫기</button>
                 </div>
             </div>
         </div>
@@ -48,8 +52,8 @@ import axios from 'axios';
 import { reactive } from '@vue/reactivity';
 export default {
     name: "Itemmodal",
-    methods:{
-        modalHide(){
+    methods: {
+        modalHide() {
             this.$emit('modalHide');
         }
     },
@@ -61,24 +65,29 @@ export default {
                 price: '',
                 imgPath: '',
                 itemCode: '',
-                itemDesc: '',
+                itemDes: '',
             }
         })
         const submit = () => {
             const args = JSON.parse(JSON.stringify(state.form));
             args.items = JSON.stringify(state.items);
-            this.$emit('modalHide');
+            
 
             axios.post("/api/v1/items", args).then(() => {
-                alert("주문 완료하였습니다.");
+                alert("상품이 추가되었습니다.");
             })
+            this.$emit('modalHide');
         }
-        return { state, submit,};
+        return { state, submit, };
     },
 }
 </script>
 
 <style scoped>
+select option[value=""][disabled] {
+    display: none;
+}
+
 #form-contorl {
     margin-left: 50px;
 }
