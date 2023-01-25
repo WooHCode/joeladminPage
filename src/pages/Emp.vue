491ada6d0550428bf052f35a15b5261b98a22475
WooHCode <gusdn526@naver.com>
2023-01-25 16:52:39 +0900

    직원 데이터 받아오기

--- src/pages/Emp.vue
+++ src/pages/Emp.vue
@@ -3,8 +3,6 @@
     <div class="row">
       <SidebarMenu></SidebarMenu>
       <main class="col-lg-10 col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-        <h1 class="h2 d-flex justify-content-center">정보</h1>
         <div
           class="d-flex col-lg-12 justify-content-center flex-wrap flex-md-nowrap align-items-center pt-6 pb-6 mb-6 border-bottom">
           <div class="album py-5 bg-light">
@@ -16,66 +14,41 @@
                 <div class="col-12">
                   <div class="card">
                     <div class="card-header">
-                      <h4 class="card-title">Striped Table with Hover</h4>
-                      <p class="card-category">Here is a subtitle for this table</p>
+                      <h4 class="card-title d-flex justify-content-center">Striped Table with Hover</h4>
+                      <p class="card-category d-flex justify-content-center">Here is a subtitle for this table</p>
                     </div>
                     <div class="card-body">
                       <div class="table-responsive">
                         <table class="table table-hover table-striped">
                           <thead>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Salary</th>
-                            <th>Country</th>
-                            <th>City</th>
+                            <th class="tb-title">번호</th>
+                            <th class="tb-title">이름</th>
+                            <th class="tb-title">나이</th>
+                            <th class="tb-title">성별</th>
+                            <th class="tb-title">출근횟수</th>
+                            <th class="tb-title">월급</th>
+                            <th class="tb-title">상세설명</th>
                           </thead>
                           <tbody>
-                            <tr>
-                              <td>1</td>
-                              <td>Dakota Rice</td>
-                              <td>$36.738</td>
-                              <td>Niger</td>
-                              <td>Oud-Turnhout</td>
-                            </tr>
-                            <tr>
-                              <td>2</td>
-                              <td>Minerva Hooper</td>
-                              <td>$23,789</td>
-                              <td>Curaçao</td>
-                              <td>Sinaai-Waas</td>
-                            </tr>
-                            <tr>
-                              <td>3</td>
-                              <td>Sage Rodriguez</td>
-                              <td>$56,142</td>
-                              <td>Netherlands</td>
-                              <td>Baileux</td>
-                            </tr>
-                            <tr>
-                              <td>4</td>
-                              <td>Philip Chaney</td>
-                              <td>$38,735</td>
-                              <td>Korea, South</td>
-                              <td>Overland Park</td>
-                            </tr>
-                            <tr>
-                              <td>5</td>
-                              <td>Doris Greene</td>
-                              <td>$63,542</td>
-                              <td>Malawi</td>
-                              <td>Feldkirchen in Kärnten</td>
+                            <tr class="" v-for="(i, idx) in state.emp" :key="idx">
+                              <td class="tb-context">{{ idx + 1 }}</td>
+                              <td class="tb-context">{{ i.empName }}</td>
+                              <td class="tb-context">{{ i.empAge }}</td>
+                              <td class="tb-context">{{ i.empGender }}</td>
+                              <td class="tb-context">{{ i.empWorkCount }}</td>
+                              <td class="tb-context">{{ i.empPay }}원</td>
+                              <td class="tb-context">{{ i.empDescription }}</td>
                             </tr>
                           </tbody>
                         </table>
                       </div>
-                    </div> 
+                    </div>
                   </div>
                 </div>
               </div>
               <div>
               </div>
             </div>
-
           </div>
         </div>
       </main>
@@ -85,12 +58,35 @@
 
 <script>
 import SidebarMenu from '@/components/SidebarMenu.vue';
+import axios from 'axios';
+import { reactive } from '@vue/reactivity';
 export default {
+  methods: {
+
+  },
   components: { SidebarMenu },
   data() {
     return {
       isModalViewed: false,
     };
   },
+  setup() {
+    const state = reactive({
+      emp: [],
+    })
+    axios.get(`/api/v1/emp`).then(({ data }) => {
+      state.emp = data;
+      console.log(state.emp)
+    })
+    return { state }
+  },
 };
 </script>
+<style scoped>
+.tb-title {
+  text-align: center;
+}
+.tb-context {
+  text-align: center;
+}
+</style>
