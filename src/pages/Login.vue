95a268646c143dc705c64bd22d6749f053af1d7c
WooHCode <gusdn526@naver.com>
2023-01-29 15:52:15 +0900

    login v-model

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -4,26 +4,53 @@
       <img class="mb-4" src="Joelfab.png" alt="" width="72" height="72">
       <div class="d-flex justify-content-center">
         <label for="inputId" class="sr-only">ID</label>
-        <input type="text" id="inputId" class="form-control" placeholder="ID" required autofocus v-model="id">
+        <input type="text" id="inputId" class="form-control" placeholder="ID" required autofocus
+          v-model="state.info.id">
       </div>
       <div class="d-flex justify-content-center mt-1">
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required v-model="password">
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required
+          v-model="state.info.password">
       </div>
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> 로그인 상태 유지
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" @click="submit()">Sign in</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { reactive } from '@vue/reactivity'
 export default {
+  setup() {
+    const state = reactive({
+      info: {
+        id: '',
+        password: '',
+      }
+    })
+    return {
+      state
+    }
+  },
+  methods: {
+    submit() {
 
+      axios.post(`/api/v1/login`, {
+        params: {
+          id: id,
+          password: password
+        }
+      }).then(({ res }) => {
+        console.log(res);
+      })
+    }
+  }
 }
 </script>
 
@@ -31,5 +58,4 @@ export default {
 .form-control {
   width: 15rem
 }
-
 </style>
