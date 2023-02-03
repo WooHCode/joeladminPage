f366e60a69417618b7b31b20c2fa276533841ee3
WooHCode <gusdn526@naver.com>
2023-02-03 18:06:45 +0900

    성별로 직원검색 페이지네이션 구현중

---
+++ src/components/MyModal2.vue
@@ -0,0 +1,57 @@
+<template>
+    <div v-if="showModal" class="modal-backdrop">
+      <div class="modal">
+        <header class="modal-header">
+          <slot name="header">Default header</slot>
+        </header>
+        <section class="modal-body">
+          <slot name="body">Default body</slot>
+        </section>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="showModal = false">Close</button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        showModal: false,
+      };
+    },
+  };
+  </script>
+  
+  <style>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.3);
+  }
+  
+  .modal {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: white;
+    padding: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    width: 80%;
+    max-width: 500px;
+  }
+  
+  .modal-header,
+  .modal-footer {
+    display: flex;
+    justify-content: flex-end;
+  }
+  </style>
--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -1,21 +1,68 @@
 <template>
-    <h1>hello</h1>
-  </template>
-  
-  <script>
-// import { reactive } from '@vue/reactivity'
-// import axios from 'axios'
-//   export default {
-//     setup() {
-//       const state = reactive({
+  <div>
+    <div class="modal" v-if="showModal">
+      <div class="modal-content">
+        <span class="close" @click="closeModal()"></span>
+        <p>Modal Content goes here</p>
+      </div>
+    </div>
+    <button class="myBtn" @click="openModal()">Open Modal</button>
+  </div>
+</template>
 
-//       })
-//       axios.get("/api/v1/post").then
-//     }
-    
-//   }
-  </script>
-  
-  <style>
-  
-  </style>
+<script>
+export default {
+  data() {
+    return {
+      showModal: false
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
