bea54852a5c52b77d0ff3b5282a54d9a04ac4cf6
WooHCode <gusdn526@naver.com>
2023-02-16 00:20:57 +0900

    update lib.js(직원수정페이지 첫 출근 시 아직 출근하지않았습니다 리턴)

--- src/scripts/lib.js
+++ src/scripts/lib.js
@@ -1,30 +1,44 @@
 export default {
-    getNumerFormatted(val) {
-        let num = ""+ val;
-        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    },
-    getGenderFormatted(val) {
-        if (val == "M") {
-            return val.replace(/M/gi, '남성');
-        }
-        else {
-            return val.toString().replace(/W/gi, '여성')
-        }
-    },
-    getSearchEmpData(val) {
-        if (val != '남성' && val != '여성') {
-            return val;
-        }
-
-        if (val == '여성') {
-            return 'W';
-        }
-        return 'M';
-    },
-    getDateFormatted(val) {
-        let date = new Date(val);
-        let options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
-        let formattedDate = date.toLocaleString('ko-KR', options).replace(/\//g, '년').replace(/d:d:d/g, '시','분','초');
-        return formattedDate;
+  getNumerFormatted(val) {
+    let num = "" + val;
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  },
+  getGenderFormatted(val) {
+    if (val == "M") {
+      return val.replace(/M/gi, "남성");
+    } else {
+      return val.toString().replace(/W/gi, "여성");
     }
-}
+  },
+  getSearchEmpData(val) {
+    if (val != "남성" && val != "여성") {
+      return val;
+    }
+
+    if (val == "여성") {
+      return "W";
+    }
+    return "M";
+  },
+  getDateFormatted(val) {
+    let date = new Date(val);
+    let options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+    let formattedDate = date
+      .toLocaleString("ko-KR", options)
+      .replace(/\//g, "년")
+      .replace(/d:d:d/g, "시", "분", "초");
+
+    if (formattedDate.charAt(0) === "1") {
+      return "아직 출근하지 않았습니다.";
+    } else {
+      return formattedDate;
+    }
+  },
+};
