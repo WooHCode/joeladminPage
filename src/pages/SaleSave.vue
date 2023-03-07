6fb3b398f621fede7b88615b7a9603796749a0e8
WooHCode <gusdn526@naver.com>
2023-03-08 00:44:21 +0900

    update saleSave

--- src/pages/SaleSave.vue
+++ src/pages/SaleSave.vue
@@ -5,24 +5,22 @@
                 <div class="d-flex justify-content-end">
                     <a class="btnback bi bi-arrow-left-circle me-5" @click="backPage()"></a>
                 </div>
-                <div class="d-flex justify-content-end">
-                    <a class="btnback bi bi-arrow-left-circle me-5" @click="test()"></a>
-                </div>
                 <h4 class="mb-3 d-flex justify-content-center">매출등록</h4>
                 <form class="validation-form" novalidate>
                     <div class="row">
                         <div class="col-md-4 mb-3">
-                            <label for="name">상품코드</label>
-                            <select class="form-control" id="age" placeholder="나이를 선택해주세요" required>
-                                <option value="null" selected disabled>상품코드를 선택하세요</option>
+                            <label for="name">상품코드</label><br>
+                            <select class="form-control" id="age" placeholder="상품코드를 선택해주세요" required
+                                v-model="selectItemCode">
+                                <option value="null" selected disabled>상품코드를 선택해주세요</option>
                                 <option v-for="i in itemCode" :key="i">{{ i }}</option>
                             </select>
                         </div>
                         <div class="col-md-4 mb-3">
-                            <label for="age">나이(세)</label>
-                            <select class="form-control" id="age" placeholder="나이를 선택해주세요" required v-model="submititem">
-                                <option value="null" selected disabled>나이를 선택하세요</option>
-                                <option v-for="i in itemNamePrice" :key="i">{{ i.itemName }}</option>
+                            <label for="age">상품명</label>
+                            <select class="form-control" id="age" placeholder="상품명을 선택해주세요" required>
+                                <option value="null" selected disabled>상품명을 선택해주세요</option>
+                                <option v-for="i in itemName" :key="i">{{ i }}</option>
                             </select>
                             <div class="invalid-feedback">
                                 나이을 입력해주세요.
@@ -31,45 +29,7 @@
                         <div class="col-md-4 mb-3">
                             <label for="count">판매수량</label>
                             <input type="number" class="form-control" id="phone" placeholder="판매수량을 입력해주세요" required>
-                            <div class="invalid-feedback">
-                                전화번호를 입력해주세요.
-                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 mb-6">
-                            <label for="phone">전화번호</label>
-                            <input type="tel" class="form-control" id="phone" placeholder="" required>
-                            <div class="invalid-feedback">
-                                전화번호를 입력해주세요.
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-6">
-                            <label for="pay">월급(원)</label>&nbsp;<i class="info fa fa-info-circle" aria-hidden="true">
-                                <small class="infoText"> 최초의 월급을 지정할수있습니다.</small></i><br />
-                            <input type="number" class="form-control" id="code" placeholder="" required>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="email">이메일</label>
-                        <input type="email" class="form-control" id="email" placeholder="you@example.com" required>
-                        <div class="invalid-feedback">
-                            이메일을 입력해주세요.
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="workDate">입사일</label><br />
-                        <input type="date" class="form-control" id="workDate" placeholder="" required>
-
-                    </div>
-                    <div class="mb-3">
-                        <label for="description">비고<span class="text-muted">&nbsp;(필수 아님)</span></label>
-                        <input type="text" class="form-control" id="description" placeholder="비고를 입력해주세요.">
-                    </div>
-                    <hr class="mb-4">
-                    <div class="mb-4 "></div>
-                    <div class="d-flex justify-content-center">
-                        <button class="btn btn-primary btn-lg btn-block" type="button">추가하기</button>
                     </div>
                 </form>
             </div>
@@ -91,15 +51,21 @@ export default {
         test() {
             alert(this.submititem);
         },
+        selectItemCode(itemCode) {
+            alert(itemCode);
+        },
     },
     data() {
         return {
+            selectedItemCode: '',
+            selectedItem: '',
+
             itemNamePrice: [],
+            itemName: [],
             itemCode: [],
             submititem: [],
         }
     },
-
     created() {
         let items = [];
         axios.get("/api/v1/saleItems").then((res) => {
@@ -112,13 +78,21 @@ export default {
                 if (!this.itemCode.includes(item.itemCode)) {
                     this.itemCode.push(item.itemCode);
                 }
+                if (!this.itemName.includes(item.itemName)) {
+                    this.itemName.push(item.itemName);
+                }
             });
+
         })
     },
 }
 </script>
 
 <style scoped>
+.btn-code {
+    margin-left: 5px;
+}
+
 .info {
     color: #92b5db;
 }
