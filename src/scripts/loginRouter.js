3f7cb671f38fbd32b3e39e6b126f7f671795d6cd
WooHCode <gusdn526@naver.com>
2023-03-22 03:07:29 +0900

    파일삭제

--- src/scripts/loginRouter.js
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from "vue-router";
-
-import Login from "@/pages/Login";
-
-const loginRouter = createRouter({
-  history: createWebHistory(),
-  routes: { path: "/login", component: Login },
-});
-export default loginRouter;
