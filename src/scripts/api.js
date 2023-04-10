b9c4b21e79763022e12fdd4a4a5d381debd5048e
WooHCode <gusdn526@naver.com>
2023-04-10 23:06:42 +0900

    내용수정

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-52-79-168-230.ap-northeast-2.compute.amazonaws.com:8080",
+  baseURL: "http://ec2-52-79-168-230.ap-northeast-2.compute.amazonaws.com:8000",
 });
 
 export default api;
